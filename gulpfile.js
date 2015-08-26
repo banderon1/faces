d4cb9a3a30ed9c4d650ab4e42003c20d93bca64f
Brian Anderson <banderson@dandb.com>
2015-08-26 10:07:09 -0700

    get angular theme working with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,137 @@
-var elixir = require('laravel-elixir');
+/* jshint node:true */
+'use strict';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var gulp = require('gulp');
+var karma = require('karma').server;
+var argv = require('yargs').argv;
+var $ = require('gulp-load-plugins')();
 
-elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.scripts([
-        'vendor/vue.min.js',
-        'vendor/vue-resource.min.js',
-    ], 'public/js/vendor.js');
-    mix.scripts([
-        'vendor/html5shiv.min.js',
-        'vendor/respond.min.js',
-    ], 'public/js/old-browser.js');
+gulp.task('styles', function() {
+  return gulp.src('resources/assets/styles/main.less')
+    .pipe($.plumber())
+    .pipe($.less())
+    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('resources/assets/scripts/**/*.js')
+    .pipe($.jshint())
+    //.pipe($.jshint.reporter('jshint-stylish'))
+    //.pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('jscs', function() {
+  return gulp.src('resources/assets/scripts/**/*.js')
+    .pipe($.jscs());
+});
+
+gulp.task('html', ['styles'], function() {
+  var lazypipe = require('lazypipe');
+  var cssChannel = lazypipe()
+    .pipe($.csso)
+    .pipe($.replace, 'bower_components/bootstrap/fonts', 'fonts');
+
+  var assets = $.useref.assets({searchPath: '{.tmp,resources/assets}'});
+
+  return gulp.src('resources/assets/**/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.ngAnnotate()))
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', cssChannel()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('resources/assets/images/**/*')
+    // .pipe($.cache($.imagemin({
+    //   progressive: true,
+    //   interlaced: true
+    // })))
+    .pipe(gulp.dest('public/images'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(require('main-bower-files')().concat('resources/assets/fonts/**/*')
+    .concat('bower_components/bootstrap/fonts/*'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('public/fonts'))
+    .pipe(gulp.dest('.tmp/fonts'));
+});
+
+gulp.task('extras', function() {
+  return gulp.src([
+    'resources/assets/*.*',
+    '!resources/assets/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('public'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'public']));
+
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/js-test/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+// inject bower components
+gulp.task('wiredep', function() {
+  var wiredep = require('wiredep').stream;
+  var exclude = [
+    'bootstrap',
+    'jquery',
+    'es5-shim',
+    'json3',
+    'angular-scenario'
+  ];
+
+  gulp.src('resources/assets/styles/*.less')
+    .pipe(wiredep())
+    .pipe(gulp.dest('resources/assets/styles'));
+
+  gulp.src('resources/assets/*.html')
+    .pipe(wiredep({exclude: exclude}))
+    .pipe(gulp.dest('app'));
+
+  gulp.src('js-test/*.js')
+    .pipe(wiredep({exclude: exclude, devDependencies: true}))
+    .pipe(gulp.dest('js-test'));
+});
+
+gulp.task('watch', [], function() {
+  $.livereload.listen();
+
+  // watch for changes
+  gulp.watch([
+    'resources/assets/**/*.html',
+    '.tmp/styles/**/*.css',
+    'resources/assets/scripts/**/*.js',
+    'resources/assets/images/**/*'
+  ]).on('change', $.livereload.changed);
+
+  gulp.watch('resources/assets/styles/**/*.less', ['styles']);
+  gulp.watch('bower.json', ['wiredep']);
+});
+
+gulp.task('buildpublic', ['jshint', 'jscs', 'html', 'images', 'fonts', 'extras'],
+  function() {
+    return gulp.src('public/**/*').pipe($.size({title: 'default', gzip: true}));
+  }
+);
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('buildpublic');
+});
+
+gulp.task('docs', [], function() {
+  return gulp.src('resources/assets/scripts/**/**')
+    .pipe($.ngdocs.process())
+    .pipe(gulp.dest('./docs'));
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,13 @@
-@extends('app')
+<!doctype html> <html class=no-js> <head> <meta charset=utf-8> <meta id=csrf value="{{ csrf_token() }}"> <meta http-equiv=X-UA-Compatible content="IE=edge"> <meta name=viewport content="width=device-width, initial-scale=1"> <title>{{ Lang::get('copy.title') }}</title> <meta name=description content="Biscuit Profile Enhancement Suite, and other keywords for ancient SEO purposes">  <link rel=stylesheet href=styles/main.css> </head> <body ng-app=yapp> <!--[if lt IE 7]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->  <div> <div ui-view></div> </div>  <script>/*
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-@section('content')
-    <div class="container">
-        <div class="content">
-            <div class="title">{{ Lang::get('copy.title') }}</div>
-        </div>
-    </div>
-    <div
-            class="fb-like"
-            data-share="true"
-            data-width="450"
-            data-show-faces="true">
-    </div>
-@endsection
+     ga('create', 'UA-XXXXX-X');
+     ga('send', 'pageview');
+     */</script> <!--[if lt IE 9]>
+<script src="scripts/oldieshim.js"></script>
+<![endif]--> <script src=scripts/vendor.js></script> <script src=scripts/scripts.js></script> </body> </html>
